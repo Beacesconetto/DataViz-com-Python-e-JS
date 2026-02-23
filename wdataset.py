ce7d354a79a272dd901cb92147b8721f370851b7
Beatriz <beacesconetto203@hotmail.com>
2026-02-23 16:02:57 -0300

    nova tabela de ganhadores

--- wdataset.py
+++ wdataset.py
@@ -3,7 +3,29 @@ import dataset
 
 db = dataset.connect('sqlite:///nobel_winners.db')
 
+'''
+Resetar a tabela ->
 wtable = db['winners']
-winners = wtable.find()
-winners = list(winners)
-print(winners)
+wtable.drop()
+
+wtable = db['winners']
+print(wtable.find())
+'''
+nobel_winners = [
+    {
+    'category': 'Physics',
+    'name': 'Albert Einstein',
+    'nationality': 'Swiss',
+    'gender': 'male',
+    'year': 1921},
+    {
+    'category': 'Chemistry',
+    'name': 'Marie Curie',
+    'nationality': 'Polish',
+    'gender': 'female',
+    'year': 1911}
+]
+
+with db as tx:
+    tx['winners'].insert_many(nobel_winners)
+print(list(db['winners'].find()))    
